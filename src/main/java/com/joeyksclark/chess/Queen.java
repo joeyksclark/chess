f29983ca090e6e2f73183c4fe88a8a84e1765594
Joey Clark <coolrockerjoey@gmail.com>
2020-04-03 23:03:32 -0700

    Queen canMove and tests

--- src/main/java/com/joeyksclark/chess/Queen.java
+++ src/main/java/com/joeyksclark/chess/Queen.java
@@ -11,7 +11,7 @@ public class Queen extends Piece {
 
     @Override
     public boolean canMove(Board board, Square destination) {
-        if(this.getLocation() == null) {
+        if(this.getLocation() == null || getLocation().equals(destination)) {
             return false;
         }
 
@@ -19,8 +19,67 @@ public class Queen extends Piece {
             return false;
         }
 
-        //custom logic
+        //if no piece at destination or opposite color
+        if(destination.getPiece() == null || destination.getPiece().getColor() == getOppositeColor()) {
+            int xdiff = getLocation().getX() - destination.getX();
+            int ydiff = getLocation().getY() - destination.getY();
 
+            //diagonals
+            if (Math.abs(xdiff) == Math.abs(ydiff)) {
+                for (int i = 0; i < Math.abs(xdiff); i++) {
+                    //right and up
+                    if (xdiff < 0 && ydiff < 0) {
+                        if (board.getSquare(getLocation().getX() + i, getLocation().getY() + i).getPiece() != null) {
+                            return false;
+                        }
+                        //right and down
+                    } else if (xdiff < 0 && ydiff > 0) {
+                        if (board.getSquare(getLocation().getX() + i, getLocation().getY() - i).getPiece() != null) {
+                            return false;
+                        }
+                        //left and down
+                    } else if (xdiff > 0 && ydiff > 0) {
+                        if (board.getSquare(getLocation().getX() - i, getLocation().getY() - i).getPiece() != null) {
+                            return false;
+                        }
+                        //left and up
+                    } else if (xdiff > 0 && ydiff < 0) {
+                        if (board.getSquare(getLocation().getX() - i, getLocation().getY() + i).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            //up and down
+            } else if (xdiff == 0 && ydiff != 0) {
+                for (int i = 0; i < Math.abs(ydiff); i++) {
+                    if (ydiff > 0) {
+                        if (board.getSquare(getLocation().getX(), getLocation().getY() - i).getPiece() != null) {
+                            return false;
+                        }
+                    } else {
+                        if (board.getSquare(getLocation().getX(), getLocation().getY() + i).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            //left and right
+            }else if(xdiff != 0 && ydiff == 0) {
+                for(int i = 0; i < Math.abs(xdiff); i++) {
+                    if(xdiff > 0) {
+                        if(board.getSquare(getLocation().getX()-i, getLocation().getY()).getPiece() != null) {
+                            return false;
+                        }
+                    } else {
+                        if(board.getSquare(getLocation().getX()+i, getLocation().getY()).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
         return false;
     }
 
--- src/test/java/com/joeyksclark/chess/QueenTest.java
+++ src/test/java/com/joeyksclark/chess/QueenTest.java
@@ -1,12 +1,140 @@
 package com.joeyksclark.chess;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QueenTest {
 
     @Test
-    public void Test() {
+    public void testCanMoveNoPieces() {
+        Board board = new Board();
+        Queen queen = new Queen(Color.WHITE);
 
+        //no location set
+        assertFalse(queen.canMove(board, board.getSquare(4,4)));
+        assertFalse(queen.canMove(board, board.getSquare(7,4)));
+        assertFalse(queen.canMove(board, board.getSquare(1,4)));
+        assertFalse(queen.canMove(board, board.getSquare(4,0)));
+        assertFalse(queen.canMove(board, board.getSquare(4,6)));
+        assertFalse(queen.canMove(board, board.getSquare(7,7)));
+        assertFalse(queen.canMove(board, board.getSquare(1,1)));
+        assertFalse(queen.canMove(board, board.getSquare(5,3)));
+        assertFalse(queen.canMove(board, board.getSquare(1,7)));
+
+        queen.setLocation(board.getSquare(4,4));
+
+        assertFalse(queen.canMove(board, board.getSquare(4,4)));
+        assertTrue(queen.canMove(board, board.getSquare(7,4)));
+        assertTrue(queen.canMove(board, board.getSquare(6,4)));
+        assertTrue(queen.canMove(board, board.getSquare(5,4)));
+        assertTrue(queen.canMove(board, board.getSquare(4,0)));
+        assertTrue(queen.canMove(board, board.getSquare(4,3)));
+        assertTrue(queen.canMove(board, board.getSquare(4,6)));
+        assertTrue(queen.canMove(board, board.getSquare(4,7)));
+        assertTrue(queen.canMove(board, board.getSquare(3,4)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,5)));
+        assertTrue(queen.canMove(board, board.getSquare(7,7)));
+        assertTrue(queen.canMove(board, board.getSquare(3,3)));
+        assertTrue(queen.canMove(board, board.getSquare(0,0)));
+        assertTrue(queen.canMove(board, board.getSquare(5,3)));
+        assertTrue(queen.canMove(board, board.getSquare(7,1)));
+        assertTrue(queen.canMove(board, board.getSquare(3,5)));
+        assertTrue(queen.canMove(board, board.getSquare(1,7)));
+
+    }
+
+    @Test
+    public void testCanMoveWithPieces() {
+        Board board = new Board();
+        Queen queen = new Queen(Color.WHITE);
+        queen.setLocation(board.getSquare(4,4));
+
+        //same color pieces
+        board.getSquare(4,6).setPiece(new Pawn(Color.WHITE));
+        board.getSquare(4,2).setPiece(new Queen(Color.WHITE));
+        board.getSquare(2,4).setPiece(new Bishop(Color.WHITE));
+        board.getSquare(6,4).setPiece(new King(Color.WHITE));
+        board.getSquare(6,6).setPiece(new Pawn(Color.WHITE));
+        board.getSquare(2,2).setPiece(new Queen(Color.WHITE));
+        board.getSquare(2,6).setPiece(new Queen(Color.WHITE));
+        board.getSquare(6,2).setPiece(new King(Color.WHITE));
+
+        assertTrue(queen.canMove(board, board.getSquare(4,5)));
+        assertFalse(queen.canMove(board, board.getSquare(4,6)));
+        assertFalse(queen.canMove(board, board.getSquare(4,7)));
+
+        assertTrue(queen.canMove(board, board.getSquare(4,3)));
+        assertFalse(queen.canMove(board, board.getSquare(4,2)));
+        assertFalse(queen.canMove(board, board.getSquare(4,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,4)));
+        assertFalse(queen.canMove(board, board.getSquare(2,4)));
+        assertFalse(queen.canMove(board, board.getSquare(0,4)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,4)));
+        assertFalse(queen.canMove(board, board.getSquare(6,4)));
+        assertFalse(queen.canMove(board, board.getSquare(7,4)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,5)));
+        assertFalse(queen.canMove(board, board.getSquare(6,6)));
+        assertFalse(queen.canMove(board, board.getSquare(7,7)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,3)));
+        assertFalse(queen.canMove(board, board.getSquare(2,2)));
+        assertFalse(queen.canMove(board, board.getSquare(1,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,3)));
+        assertFalse(queen.canMove(board, board.getSquare(6,2)));
+        assertFalse(queen.canMove(board, board.getSquare(7,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,5)));
+        assertFalse(queen.canMove(board, board.getSquare(2,6)));
+        assertFalse(queen.canMove(board, board.getSquare(1,7)));
+
+        //opposite color pieces
+        board.getSquare(4,6).setPiece(new King(Color.BLACK));
+        board.getSquare(4,2).setPiece(new Knight(Color.BLACK));
+        board.getSquare(2,4).setPiece(new Queen(Color.BLACK));
+        board.getSquare(6,4).setPiece(new Pawn(Color.BLACK));
+        board.getSquare(6,6).setPiece(new King(Color.BLACK));
+        board.getSquare(2,2).setPiece(new Knight(Color.BLACK));
+        board.getSquare(2,6).setPiece(new Queen(Color.BLACK));
+        board.getSquare(6,2).setPiece(new Rook(Color.BLACK));
+
+        assertTrue(queen.canMove(board, board.getSquare(4,5)));
+        assertTrue(queen.canMove(board, board.getSquare(4,6)));
+        assertFalse(queen.canMove(board, board.getSquare(4,7)));
+
+        assertTrue(queen.canMove(board, board.getSquare(4,3)));
+        assertTrue(queen.canMove(board, board.getSquare(4,2)));
+        assertFalse(queen.canMove(board, board.getSquare(4,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,4)));
+        assertTrue(queen.canMove(board, board.getSquare(2,4)));
+        assertFalse(queen.canMove(board, board.getSquare(0,4)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,4)));
+        assertTrue(queen.canMove(board, board.getSquare(6,4)));
+        assertFalse(queen.canMove(board, board.getSquare(7,4)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,5)));
+        assertTrue(queen.canMove(board, board.getSquare(6,6)));
+        assertFalse(queen.canMove(board, board.getSquare(7,7)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,3)));
+        assertTrue(queen.canMove(board, board.getSquare(2,2)));
+        assertFalse(queen.canMove(board, board.getSquare(1,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(5,3)));
+        assertTrue(queen.canMove(board, board.getSquare(6,2)));
+        assertFalse(queen.canMove(board, board.getSquare(7,1)));
+
+        assertTrue(queen.canMove(board, board.getSquare(3,5)));
+        assertTrue(queen.canMove(board, board.getSquare(2,6)));
+        assertFalse(queen.canMove(board, board.getSquare(1,7)));
     }
 }
